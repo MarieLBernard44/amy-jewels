d52c49e859da6d209d23680058708f1bf719917c
MarieLBernard44 <marielouise.bernard44@gmail.com>
2018-03-26 15:12:55 +0100

    Ajout des patterns dans le lab

--- src/app/pages/personnalisation/lab-pattern/lab-pattern.component.ts
+++ src/app/pages/personnalisation/lab-pattern/lab-pattern.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lab-pattern',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabPatternComponent implements OnInit {
 
-  constructor() { }
+  patternBijouForm: FormGroup;
+  valPattern: string;
+
+  constructor(private fb: FormBuilder) {
+    this.valPattern = '';
+  }
 
   ngOnInit() {
+    this.validationPatternBijouForm();
+  }
+
+  validationPatternBijouForm() {
+    this.patternBijouForm = this.fb.group({
+      pattern: ['', Validators.required]
+    });
+  }
+
+  valeurPattern(event) {
+    // RECUPERATION DE LA REFERENCE DU PATTERN
+    this.valPattern = event.target.value;
+    console.log(this.valPattern);
   }
 
 }
